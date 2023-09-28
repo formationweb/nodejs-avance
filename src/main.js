9857f7d31191c8623c923b54d1bb588f53d70749
RSamaium <sam@webcreative5.net>
2023-09-28 16:44:05 +0200

    post

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import express from 'express'
 import database from './database.js'
 
-database.search('to').then(console.log)
-
 const app = express()
+
+app.use(express.json())
+
+app.post('/users', (req, res, next) => {
+    console.log(req.body)
+    next(new Error('erreur'))
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send('Erreur')
 })
 
 app.listen(3000, () => {
